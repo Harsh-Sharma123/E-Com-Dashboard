48fdd1acf34328ed36e07239658094271b3bc780
Harsh Sharma <hs804506@gmail.com>
2024-11-16 19:57:15 +0530

    Signup form ui completed

--- front-end/src/components/Signup.js
+++ front-end/src/components/Signup.js
@@ -1,14 +1,28 @@
 
-import React from 'react';
+import React, { useState } from 'react';
 
 const Signup = () => {
+    
+    const [name, setName] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+
+    const handleSubmit = () => {
+        console.log(name);
+        console.log(email);
+        console.log(password);
+    }
+    
     return (
         <div className='maxOuter'>
             <div className='signupForm'>
                 <h1>Register / Sign Up Form</h1>
-                <input className='inputBox' type="text" placeholder="Enter your name" />
-                <input className='inputBox' type="text" placeholder="Enter your email" />
-                <input className='inputBox' type="password" placeholder="Enter your password" />
+                <div className='inputWrapper'>
+                    <input className='inputBox' type="text" placeholder="Enter your name" onChange={(e)=>setName(e.target.value)} />
+                    <input className='inputBox' type="text" placeholder="Enter your email" onChange={(e)=>setEmail(e.target.value)} />
+                    <input className='inputBox' type="password" placeholder="Enter your password" onChange={(e)=>setPassword(e.target.value)} />
+                    <button type="button" className='signUpButton' onClick={handleSubmit}>Sign Up</button>
+                </div>
             </div>
         </div>
     )
