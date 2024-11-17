a18996e1cabf1d2291ee49c110b4c012f8bc8d11
Harsh Sharma <hs804506@gmail.com>
2024-11-17 22:54:32 +0530

    connecting mongodb to node

--- backend/index.js
+++ backend/index.js
@@ -1,6 +1,20 @@
 const express = require("express");
 
+const mongoose = require("mongoose");
+
 const app = express();
+
+const connectDB = async () => {
+    mongoose.connect("mongodb://localhost:27017/db")
+
+    // schema
+    const productSchema = new mongoose.Schema({});
+
+    // model
+    const product = mongoose.model('product', productSchema);
+    const data = await product.find();
+    console.log(data);
+}
 
 app.get("/", (req, res) => {
     res.send("App is Working !!");
