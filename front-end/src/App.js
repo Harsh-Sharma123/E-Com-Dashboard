743d73087c975e23e7901dd86ad54af6d97161e0
Harsh Sharma <hs804506@gmail.com>
2024-11-18 23:02:49 +0530

    logout feature added

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,6 +4,7 @@ import Navbar from './components/Navbar';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Footer from './components/Footer';
 import Signup from './components/Signup';
+import PrivateComponent from './components/PrivateComponent';
 
 function App() {
   return (
@@ -11,12 +12,14 @@ function App() {
       <BrowserRouter>
         <Navbar />
         <Routes>
-          <Route path="/" element={<h1>Product Listing Component</h1>} />
-          <Route path="/add" element={<h1>Add Product Component</h1>} />
-          <Route path="/update" element={<h1>Update Product Component</h1>} />
-          <Route path="/logout" element={<h1>Logout Component</h1>} />
-          <Route path="/profile" element={<h1>Profile Component</h1>} />
-          <Route path="/signup" element={<Signup />} />
+          <Route element={<PrivateComponent />}>
+            <Route path="/" element={<h1>Product Listing Component</h1>} />
+            <Route path="/add" element={<h1>Add Product Component</h1>} />
+            <Route path="/update" element={<h1>Update Product Component</h1>} />
+            <Route path="/logout" element={<h1>Logout Component</h1>} />
+            <Route path="/profile" element={<h1>Profile Component</h1>} />
+          </Route>
+            <Route path="/signup" element={<Signup />} />
         </Routes>
         <Footer />
       </BrowserRouter>
